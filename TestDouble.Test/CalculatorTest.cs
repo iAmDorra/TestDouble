5709ff735c292716347dbd0cb15072fbf0da6e91
Dorra BARTAGUIZ <dorra.bartaguiz@arolla.fr>
2018-07-05 20:06:04 +0200

    Initialize the project with a dummy test double

---
+++ TestDouble.Test/CalculatorTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestDouble.Test
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        public void Should_throw_an_exception_when_Divide_by_zero()
+        {
+            Calculator calculator = new Calculator();
+            int numerator = 3;
+            int denominator = 0;
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                calculator.Divide(numerator, denominator);
+            });
+        }
+    }
+}
---
+++ TestDouble/Calculator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TestDouble
+{
+    public class Calculator
+    {
+        public double Divide(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new InvalidOperationException("Can't divide by zero !");
+            }
+
+            double result = (double)numerator / (double)denominator;
+            return result;
+        }
+    }
+}
